0cf772bcf05eab63e9b383baf5d43337754ce136
rithvix <cs25b073@smail.iitm.ac.in>
2025-10-18 01:29:42 +0530

    add create.c

--- create.c
+++ create.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 
 int main(){
-    printf("Hello , i am trying to understand what track does\n");
+    printf("Hello , i am trying to understand what track(change1) does\n");
 }
+// changing now
+
+
